a3750cc166169802c3a751342366e548130c1964
boiadeira <161638593+boiadeira@users.noreply.github.com>
2025-05-06 16:13:37 -0300

    06

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
 
 for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick=function(){}
-    
+    botoes[i].onclick=function(){
+    botoes[i].classList.add("ativo");
+}
 }
