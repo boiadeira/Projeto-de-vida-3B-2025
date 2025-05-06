94a908cb4630943b86a8acbd02e1b5036a39ae3e
boiadeira <161638593+boiadeira@users.noreply.github.com>
2025-05-06 16:19:55 -0300

    06

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
 
-for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick=function(){
-    botoes[i].classList.add("ativo");
-}
+for (let i = 0; i < botoes.length; i++) {
+
+    for (lt j = 0; j < botoes.length; j++) {
+        botoes[j].classList.remove("aitvo");
+    }
+
+    botoes[i].onclick = function () {
+        botoes[i].classList.add("ativo");
+    }
 }
